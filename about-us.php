9d84dc5cb10d7b9e929847eb94a54832fc67eea5
erik gonzalez <eriklemus25@gmail.com>
2019-01-08 10:47:03 -0700

    feature/about-us: Created section-1 whit all styles.

---
+++ about-us.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include('inc/html.head.inc'); ?>
+    <title>HomePage | PHP Project 1</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+  </head>
+
+  <body>
+    <?php include('inc/header.inc'); ?>
+    <?php include('inc/about-us/section-1.inc'); ?>
+    <?php include('inc/about-us/section-2.inc'); ?>
+    <?php include('inc/about-us/section-3.inc'); ?>
+
+    <?php include('inc/footer.inc'); ?>
+  </body>
+</html>
